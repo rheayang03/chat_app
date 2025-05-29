b4bd276572dce7ae3c56c9cc8d7d44847774dc89
rheayang03 <rheamaenavares7@gmail.com>
2025-05-30 02:52:55 +0800

    Reading data from JSONFile

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,45 +1,51 @@
-import 'package:chat_app/models/chat_message_entity.dart';
+import 'dart:convert';
+
+import 'package:chat_app/chat_message_entity.dart';
 import 'package:chat_app/chat_bubble.dart';
 import 'package:chat_app/chat_input.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
    ChatPage({Key? key}) : super(key: key);
 
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
 
-  final List<ChatMessageEntity> _messages = [
-    ChatMessageEntity(
-      author: Author(username: 'Elton Bernil'),
-      createdAt: DateTime.now().microsecondsSinceEpoch,
-      id: '1',
-      text: 'First Text',
-      imageUrl: '', // Provide a valid image URL or leave empty
+class _ChatPageState extends State<ChatPage> {
+  //Inialize the list of messages
+   List<ChatMessageEntity> _messages = [];
+  
 
-    ),
+  _loadInitialMessages() async{
+    final response = await rootBundle.loadString('assets/mock_messages.json');
     
-    ChatMessageEntity(
-      author: Author(username: 'Elton Bernil'),
-      createdAt: DateTime.now().microsecondsSinceEpoch,
-      id: '2',
-      text: 'Second Text',
-      imageUrl: 'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-    ),
+    final List <dynamic> decodeList = jsonDecode(response) as List;
+    
+    final List<ChatMessageEntity> _chatMessages = decodeList.map((listItem) {
+      return ChatMessageEntity.fromJson(listItem);
+    }). toList();
+        
+    print(_chatMessages.length);
+    
+    //Finally, add the messages to the list
+    setState(() {
+      _messages = _chatMessages;
+    });
+  }
 
-    ChatMessageEntity(
-      author: Author(username: 'Elli'),
-      createdAt: DateTime.now().microsecondsSinceEpoch,
-      id: '3',
-      text: 'Third Text',
-      imageUrl: '',
-    ),
-  ];
-
+  @override
+  void initState() {
+    _loadInitialMessages();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
 
-
+    _loadInitialMessages();
 
     final userName = ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
--- lib/models/chat_message_entity.dart
+++ lib/models/chat_message_entity.dart
@@ -12,12 +12,27 @@ class ChatMessageEntity {
     required this.createdAt,
     required this.author,
   });
+  
+  factory ChatMessageEntity.fromJson(Map<String, dynamic> json) {
+  return ChatMessageEntity(
+    text: json['text'] ?? '',
+    id: json['id'] ?? '',
+    createdAt: DateTime.tryParse(json['createdAt'] ?? '')?.millisecondsSinceEpoch ?? 0,
+    author: Author.fromJson(json['author']),
+    imageUrl: json['image'] ?? '', // fallback to empty string if not present
+  );
+}
+
 }
 
 class Author {
   String username;
 
-  Author({
-    required this.username,
-  });
+  Author({required this.username});
+
+  factory Author.fromJson(Map<String, dynamic> json) {
+    return Author(
+      username: json['name'] ?? '', // use 'name' instead of 'username'
+    );
+  }
 }
